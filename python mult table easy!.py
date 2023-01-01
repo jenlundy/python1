777c2baaeb5ee8666531cffe41ad5f293d8b8f22
Jennifer Lundy <114626995+jenlundy@users.noreply.github.com>
2023-01-01 08:29:41 -0500

    Add files via upload

---
+++ python mult table easy!.py
@@ -0,0 +1,12 @@
+print("whats up Jennifer Lundy")
+# multiplication table from (1 to 10) in Python chnage the number and it will do any mult table
+
+num = 1000
+
+for i in range(1,11):
+    print(num, 'x',i, '=', num*i)
+    
+num = 8
+
+for i in range (1,10):
+    print(num, 'x',i, '=', num*i)
